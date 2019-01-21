c2c809c0b8c9fc05981f9d24e0ae91b6eaab6687
Marc Chantreux <khatar@phear.org>
2019-01-21 10:10:04 +0100

    add tarea()
    a function to create html textareas. bad name: need at least a namespace

--- lib/page.zsh
+++ lib/page.zsh
@@ -5,3 +5,19 @@ page/ok    () http/header 200 text/html  "$@"
 page/text  () http/header 200 text/plain "$@"
 page/error () http/header 500 text/html  "$@"
 
+# exemple:
+#
+# tarea 33x45 'class="those are classes"' <<-\.
+# 	this is the least i can do ...
+# 	    
+# 	welcome to shell scripting
+# 	.
+
+tarea () {
+	local  rows cols
+	IFS=x read rows cols <<< $1
+	shift
+	print "<input rows=\"$rows\" cols=\"$cols\"${*+ $*}>"
+	cat
+	print "</input>"
+}
